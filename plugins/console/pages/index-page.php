c84f8d30cfaca5c1aa7a2bf4f0ec18fc59bee506
webchimp <rodrigo.tejero@thewebchi.mp>
2015-10-15 08:41:58 -0500

    First push

---
+++ plugins/console/pages/index-page.php
@@ -0,0 +1,69 @@
+<?php
+	$default = 'echo "Hello World";';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php echo $site->getPageTitle(); ?></title>
+
+	<?php $site->metaTags(); ?>
+
+	<link rel="shortcut icon" href="<?php $site->img('favicon.ico'); ?>">
+	<link rel="icon" href="<?php $site->img('favicon.png'); ?>" type="image/png">
+	<?php $site->includeStyles(); ?>
+	<?php $site->includeScript('modernizr'); ?>
+	<?php $site->includeScript('respond'); ?>
+	<?php $site->includeScriptVars(); ?>
+</head>
+<body class="<?php $site->bodyClass() ?>">
+
+		<div class="console-wrapper">
+			<header class="console-header">
+				<div class="boxfix-vert">
+					<div class="margins">
+						<h2><i class="fa fa-fw fa-code"></i> Console</h2>
+					</div>
+				</div>
+			</header>
+			<section>
+				<div class="boxfix-vert">
+					<div class="margins">
+						<div class="row row-10">
+							<div class="col col-6">
+								<form id="form-code" class="form-code" action="" method="post">
+									<div class="form-group codemirror" data-mode="text/x-php">
+										<textarea name="code" id="code"><?php echo $default; ?></textarea>
+									</div>
+									<div class="text-right">
+										<button type="submit" class="button button-primary">Execute</button>
+									</div>
+								</form>
+							</div>
+							<div class="col col-6">
+								<div class="box box-output">
+									<div class="console-output"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<div class="console-push"></div>
+		</div>
+
+		<footer class="console-footer">
+			<div class="boxfix-vert">
+				<div class="margins">
+					<div class="footer-copyright">
+						<small class="cf">
+							<span class="copyright-left">Copyright &copy; 2015 biohzrdmx</span>
+							<span class="copyright-right">Made with <strong>Hummingbird</strong> &amp; <strong>Chimplate</strong> and tons of <i class="fa fa-heart"></i></span>
+						</small>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	<?php $site->includeScripts(); ?>
+</body>
+</html>
---
+++ plugins/console/plugin.php
@@ -0,0 +1,80 @@
+<?php
+
+	/**
+	 * Hummingbird Console
+	 *
+	 * @version 0.1
+	 * @author  biohzrdmx <github.com/biohzrdmx>
+	 * @uses    MVC
+	 * @license MIT
+	 */
+
+	// ============================================================================================
+	//   Console Implementation
+	// ============================================================================================
+
+	class Console {
+
+		function __construct() {
+
+			global $site;
+
+			$site->registerStyle('google-fonts', '//fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic,300,300italic|Raleway|Oswald:400,300' );
+			$site->registerStyle('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css' );
+
+			$site->registerStyle('console', $site->baseUrl('/plugins/console/css/console.css') );
+
+			$site->registerStyle('codemirror', $site->baseUrl('/plugins/console/css/codemirror.css') );
+			$site->registerStyle('codemirror.monokai', $site->baseUrl('/plugins/console/css/codemirror.monokai.css'), array('codemirror') );
+			$site->registerStyle('console', $site->baseUrl('/plugins/console/css/console.css'), array('google-fonts', 'font-awesome', 'codemirror.monokai') );
+
+			$site->registerScript('codemirror', $site->baseUrl('/plugins/console/js/codemirror.min.js') );
+			$site->registerScript('console', $site->baseUrl('/plugins/console/js/console.js'), array('jquery', 'codemirror', 'jquery.form') );
+		}
+	}
+
+	class ConsoleView extends View {
+		function init() {
+
+			global $site;
+			$this->pages_dir = $site->baseDir('/plugins/console/pages');
+			$this->parts_dir = $site->baseDir('/plugins/console/parts');
+		}
+	}
+
+	class ConsoleController extends Controller {
+
+		public $view;
+
+		function init() {
+			$this->view = new ConsoleView();
+		}
+
+		function indexAction() {
+			global $site;
+			$request = $site->mvc->getRequest();
+			switch ($request->type) {
+				case 'get':
+					$site->enqueueStyle('console');
+					$site->enqueueScript('console');
+					$this->view->render('/index-page');
+					break;
+				case 'post':
+					$code = $request->post('code');
+					$code = preg_replace('/^\s*\<\?php/', '', $code);
+					$code = preg_replace('/\?\>\s*$/', '', $code);
+					eval($code);
+					break;
+			}
+		}
+
+		function showAction($id) {
+			global $site;
+			$site->enqueueStyle('console');
+			$site->enqueueScript('console');
+			$this->view->render('/index-page');
+		}
+	}
+
+	$console = new Console();
+?>
