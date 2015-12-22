18dd7b073f16267c4b65cee6f34c7d50fca51cd4
webchimp <rodrigo.tejero@thewebchi.mp>
2015-12-22 15:32:27 -0600

    Removed double register

--- plugins/console/plugin.php
+++ plugins/console/plugin.php
@@ -22,10 +22,9 @@
 			$site->registerStyle('google-fonts', '//fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic,300,300italic|Raleway|Oswald:400,300' );
 			$site->registerStyle('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css' );
 
-			$site->registerStyle('console', $site->baseUrl('/plugins/console/css/console.css') );
-
 			$site->registerStyle('codemirror', $site->baseUrl('/plugins/console/css/codemirror.css') );
 			$site->registerStyle('codemirror.monokai', $site->baseUrl('/plugins/console/css/codemirror.monokai.css'), array('codemirror') );
+
 			$site->registerStyle('console', $site->baseUrl('/plugins/console/css/console.css'), array('google-fonts', 'font-awesome', 'codemirror.monokai') );
 
 			$site->registerScript('codemirror', $site->baseUrl('/plugins/console/js/codemirror.min.js') );
